c427afd55f8e2e8679e98fa44a33f1326a25407a
AndreLss <andre_luis98@outlook.com>
2020-10-30 08:02:11 -0300

    configura variaveis do ambiente de dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const backendURL = 'http://localhost:8085';
+
 export const environment = {
-  production: false
+  production: false,
+  backendURL,
+  graphQL: `${backendURL}/api`
 };
 
 /*
