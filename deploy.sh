ffe6fd9fe76ef023fb227bb832405530b4f80f82
AndreLss <andre_luis98@outlook.com>
2020-11-06 11:08:52 -0300

    Ajusta script para deploy do site

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@
 
 rm -rf /dist
 ng build --prod
+gzip -vrk dist/lenofx-dashboard
+scp -r -i ~/.ssh/Leno-Key_Pair.pem dist/lenofx-dashboard ubuntu@lenofx-painel:~/
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-const backendURL = 'http://localhost:8085';
+const backendURL = 'http://3.23.61.244:8085';
 
 export const environment = {
   production: true,
