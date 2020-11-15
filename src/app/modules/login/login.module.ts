81eac2dd39bfbd552684dc81caae6be23a2c6fb8
AndreLss <andre_luis98@outlook.com>
2020-11-15 15:46:23 -0300

    cria forms de login e signup

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -10,8 +17,14 @@ import { LoginComponent } from './login.component';
     LoginComponent
   ],
   imports: [
+    FormsModule,
     CommonModule,
-    LoginRoutingModule
+    MatTabsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    LoginRoutingModule,
+    MatFormFieldModule,
   ]
 })
 export class LoginModule { }
