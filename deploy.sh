5b3fd4bbe1972371c2e4d26bbd244b30df92f8a1
ANDRÉ <andreluiss@ufg.br>
2021-04-15 14:27:57 -0300

    Otimiza build do projeto

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf /dist
-ng build --prod
+ng build --prod --aot --build-optimizer
 gzip -vrk dist/lenofx-dashboard
 scp -r -i ~/.ssh/Leno-Key_Pair.pem dist/lenofx-dashboard ubuntu@lenofx-painel:~/
